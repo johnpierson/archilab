5750ea328b4120e692e91dd51893bdfc8fa7ed62
john pierson <john.pierson@autodesk.com>
2026-02-26 10:12:01 -0700

    Update ElementIdExtensions.cs

--- archilabSharedProject/Revit/Utils/ElementIdExtensions.cs
+++ archilabSharedProject/Revit/Utils/ElementIdExtensions.cs
@@ -1,3 +1,4 @@
+using Autodesk.DesignScript.Runtime;
 using Autodesk.Revit.DB;
 
 namespace archilab.Revit.Utils
@@ -5,7 +6,8 @@ namespace archilab.Revit.Utils
     /// <summary>
     /// Extension methods for ElementId to support both IntegerValue (pre-2026) and Value (2026+).
     /// </summary>
-    public static class ElementIdExtensions
+    [IsVisibleInDynamoLibrary(false)]
+    internal static class ElementIdExtensions
     {
         /// <summary>
         /// Returns the integer value of the ElementId. Uses Value in Revit 2026+, IntegerValue otherwise.
